e87adcf3c29306cd58b51bfe9f5dee1857637324
Paulo Diaz <paulodiazg32@gmail.com>
2023-03-07 16:52:00 -0800

    Fixed logic for expand mapping

--- src/components/faq/CollectorQuestions.jsx
+++ src/components/faq/CollectorQuestions.jsx
@@ -9,56 +9,41 @@ import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import { styled } from '@mui/material/styles';
 
-const CollectorsData = [
-  
-  {
- 
-title: "What is Artboardz?",
-description: "The mint price for each Artboardz collections varies depending of factors including who the artist is, their location, the size of the collection and others",
-ref: "q1"
-},
-{
-  title: "What is the Artboardz mint price?",
-  description: "The mint price for each Artboardz collections varies depending of factors including who the artist is, their location, the size of the collection and others",
-  ref: "q2"
-},
 
-{
-  title: "What wallets are compatible with Artboardz platform",
-description: "The mint price for each Artboardz collections varies depending of factors including who the artist is, their location, the size of the collection and others",
-ref: "q3"
-},
-{
-  title: "How can I buy Artboardz NFTs?",
-  description: "The mint price for each Artboardz collections varies depending of factors including who the artist is, their location, the size of the collection and others",
-  ref: "q4"
-},
-{
-  title: "How do the Leaderboardz work?",
-  description: "The mint price for each Artboardz collections varies depending of factors including who the artist is, their location, the size of the collection and others",
-  ref: "q5"
-},
-{
-title: "How can I contact Artboardz if I run into any issues?",
-description: "The mint price for each Artboardz collections varies depending of factors including who the artist is, their location, the size of the collection and others",
-ref: "q6"
-}
-,
-{
-  title: "What is policy on sharing personal information?",
-  description: "The mint price for each Artboardz collections varies depending of factors including who the artist is, their location, the size of the collection and others",
-  ref: "q7"
-}
-
-
-];
 
 
 const CollectorQuestions = () => {
-    const [expanded, setExpanded] = React.useState(false);
-    const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
+
+  const [expanded, setExpanded] = React.useState(false);
+  const [expanded2, setExpanded2] = React.useState(false);
+  const [expanded3, setExpanded3] = React.useState(false);
+  const [expanded4, setExpanded4] = React.useState(false);
+  const [expanded5, setExpanded5] = React.useState(false);
+  const [expanded6, setExpanded6] = React.useState(false);
+  const [expanded7, setExpanded7] = React.useState(false);
+  const handleExpandClick = () => {
+  setExpanded(!expanded);
+};
+
+const handleExpandClick2 = () => {
+  setExpanded2(!expanded2);
+};
+const handleExpandClick3 = () => {
+  setExpanded3(!expanded3);
+};
+const handleExpandClick4 = () => {
+  setExpanded4(!expanded4);
+};
+const handleExpandClick5 = () => {
+  setExpanded5(!expanded5);
+};
+const handleExpandClick6 = () => {
+  setExpanded6(!expanded6);
+};
+const handleExpandClick7 = () => {
+  setExpanded7(!expanded7);
+};
+
   const ExpandMore = styled((props) => {
     const { expand, ...other } = props;
     return <IconButton {...other} />;
@@ -70,17 +55,74 @@ const CollectorQuestions = () => {
     }),
   }));
   
+  const CollectorsData = [
+  
+    {
+   
+  title: "What is Artboardz?",
+  description: "The mint price for each Artboardz collections varies depending of factors including who the artist is, their location, the size of the collection and others",
+  ref: "q1",
+  handleClick: handleExpandClick,
+  expanded: expanded
+  },
+  {
+    title: "What is the Artboardz mint price?",
+    description: "The mint price for each Artboardz collections varies depending of factors including who the artist is, their location, the size of the collection and others",
+    ref: "q2",
+    handleClick: handleExpandClick2,
+    expanded: expanded2
+  },
+  
+  {
+    title: "What wallets are compatible with Artboardz platform",
+  description: "The mint price for each Artboardz collections varies depending of factors including who the artist is, their location, the size of the collection and others",
+  ref: "q3",
+  handleClick: handleExpandClick3,
+  expanded: expanded3
+  },
+  {
+    title: "How can I buy Artboardz NFTs?",
+    description: "The mint price for each Artboardz collections varies depending of factors including who the artist is, their location, the size of the collection and others",
+    ref: "q4",
+    handleClick: handleExpandClick4,
+    expanded: expanded4
+  },
+  {
+    title: "How do the Leaderboardz work?",
+    description: "The mint price for each Artboardz collections varies depending of factors including who the artist is, their location, the size of the collection and others",
+    ref: "q5",
+    handleClick: handleExpandClick5,
+    expanded: expanded5
+  },
+  {
+  title: "How can I contact Artboardz if I run into any issues?",
+  description: "The mint price for each Artboardz collections varies depending of factors including who the artist is, their location, the size of the collection and others",
+  ref: "q6",
+  handleClick: handleExpandClick6,
+  expanded: expanded6
+  }
+  ,
+  {
+    title: "What is policy on sharing personal information?",
+    description: "The mint price for each Artboardz collections varies depending of factors including who the artist is, their location, the size of the collection and others",
+    ref: "q7",
+    handleClick: handleExpandClick7,
+    expanded: expanded7
+  }
+  
+  
+  ];
   const displayQuestions = CollectorsData.map((question, index) => {
     return (
       
         
-        <Box key={index} onClick={handleExpandClick} sx={{ width:{xl: '85vw', md: '60vw'}, display: 'flex',
+        <Box key={index} onClick={question.handleClick} sx={{ width:{xl: '85vw', md: '60vw'}, display: 'flex',
         flexDirection: 'row', justifyContent:'center'}}>
       <Card sx={{ border:1, borderColor:"#6E028F" , background:'transparent',  boxShadow: 0, marginY:'10px',}}>
       <ExpandMore
-        expand={expanded}
-        onClick={handleExpandClick}
-        aria-expanded={expanded}
+        expand={question.expanded}
+        onClick={question.handleClick}
+        aria-expanded={question.expanded}
         aria-label="show more"
       >
          
@@ -92,7 +134,7 @@ const CollectorQuestions = () => {
     </CardContent>
 
     <CardActions disableSpacing    sx={{m:'auto', width:'30px', marginX:'20px'}}>
-     {expanded?   <RemoveIcon sx={{ color: 'white', m:'auto'  }}/>:  <AddIcon sx={{ color: 'white', m:'auto'  }}/>}
+     {question.expanded?   <RemoveIcon sx={{ color: 'white', m:'auto'  }}/>:  <AddIcon sx={{ color: 'white', m:'auto'  }}/>}
        
       
      
@@ -103,7 +145,7 @@ const CollectorQuestions = () => {
   
     
     </ExpandMore> 
-    <Collapse in={expanded} timeout="auto" unmountOnExit>
+    <Collapse in={question.expanded} timeout="auto" unmountOnExit>
    
       <CardContent>
    
--- src/components/faq/CreatorQuestions.jsx
+++ src/components/faq/CreatorQuestions.jsx
@@ -9,45 +9,75 @@ import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import { styled } from '@mui/material/styles';
 
-const CreatorsData = [
-  
-  {
- 
-title: "What is Artboardz?",
-description: "The mint price for each Artboardz collections varies depending of factors including who the artist is, their location, the size of the collection and others",
-ref: "q1"
-},
-{
-  title: "What kind of creators does Artbordz work with?",
-  description: "The mint price for each Artboardz collections varies depending of factors including who the artist is, their location, the size of the collection and others",
-  ref: "q2"
-},
-
-{
-  title: "Do I need to be an NFT creator to join Artboardz?",
-description: "The mint price for each Artboardz collections varies depending of factors including who the artist is, their location, the size of the collection and others",
-ref: "q3"
-},
-{
-  title: "What does  the cration process entail?",
-  description: "The mint price for each Artboardz collections varies depending of factors including who the artist is, their location, the size of the collection and others",
-  ref: "q4"
-},
-{
-  title: "How do I become a creator with Artboardz?",
-description: "The mint price for each Artboardz collections varies depending of factors including who the artist is, their location, the size of the collection and others",
-ref: "q5"
-},
-
-
-];
 
 
 const CreatorQuestions = () => {
     const [expanded, setExpanded] = React.useState(false);
+    const [expanded2, setExpanded2] = React.useState(false);
+    const [expanded3, setExpanded3] = React.useState(false);
+    const [expanded4, setExpanded4] = React.useState(false);
+    const [expanded5, setExpanded5] = React.useState(false);
     const handleExpandClick = () => {
     setExpanded(!expanded);
   };
+
+  const handleExpandClick2 = () => {
+    setExpanded2(!expanded2);
+  };
+  const handleExpandClick3 = () => {
+    setExpanded3(!expanded3);
+  };
+  const handleExpandClick4 = () => {
+    setExpanded4(!expanded4);
+  };
+  const handleExpandClick5 = () => {
+    setExpanded5(!expanded5);
+  };
+
+  const CreatorsData = [
+  
+    {
+   
+  title: "What is Artboardz?",
+  description: "The mint price for each Artboardz collections varies depending of factors including who the artist is, their location, the size of the collection and others",
+  ref: "q1",
+  handleClick: handleExpandClick,
+  expanded: expanded,
+
+},
+
+  {
+    title: "What kind of creators does Artbordz work with?",
+    description: "The mint price for each Artboardz collections varies depending of factors including who the artist is, their location, the size of the collection and others",
+    ref: "q2",
+    handleClick: handleExpandClick2,
+   
+expanded: expanded2  },
+  
+  {
+    title: "Do I need to be an NFT creator to join Artboardz?",
+  description: "The mint price for each Artboardz collections varies depending of factors including who the artist is, their location, the size of the collection and others",
+  ref: "q3",
+  handleClick: handleExpandClick3,
+ 
+expanded: expanded3},
+  {
+    title: "What does  the cration process entail?",
+    description: "The mint price for each Artboardz collections varies depending of factors including who the artist is, their location, the size of the collection and others",
+    ref: "q4",
+    handleClick: handleExpandClick4,
+   
+expanded: expanded4  },
+  {
+    title: "How do I become a creator with Artboardz?",
+  description: "The mint price for each Artboardz collections varies depending of factors including who the artist is, their location, the size of the collection and others",
+  ref: "q5",
+  handleClick: handleExpandClick5,  
+expanded: expanded5
+},
+  
+  
+  ];
   const ExpandMore = styled((props) => {
     const { expand, ...other } = props;
     return <IconButton {...other} />;
@@ -63,13 +93,13 @@ const CreatorQuestions = () => {
     return (
       
         
-        <Box key={index} onClick={handleExpandClick} sx={{ width:{xl: '85vw', md: '60vw'}, display: 'flex',
+        <Box key={index} onClick={question.handleClick} sx={{ width:{xl: '85vw', md: '60vw'}, display: 'flex',
         flexDirection: 'row', justifyContent:'center'}}>
       <Card sx={{ border:1, borderColor:"#6E028F" , background:'transparent',  boxShadow: 0, marginY:'10px',}}>
       <ExpandMore
-        expand={expanded}
-        onClick={handleExpandClick}
-        aria-expanded={expanded}
+        expand={question.expanded}
+        onClick={question.handleClick}
+        aria-expanded={question.expanded}
         aria-label="show more"
       >
          
@@ -81,7 +111,7 @@ const CreatorQuestions = () => {
     </CardContent>
 
     <CardActions disableSpacing    sx={{m:'auto', width:'30px', marginX:'20px'}}>
-     {expanded?   <RemoveIcon sx={{ color: 'white', m:'auto'  }}/>:  <AddIcon sx={{ color: 'white', m:'auto'  }}/>}
+     {question.expanded?   <RemoveIcon sx={{ color: 'white', m:'auto'  }}/>:  <AddIcon sx={{ color: 'white', m:'auto'  }}/>}
        
       
      
@@ -92,7 +122,7 @@ const CreatorQuestions = () => {
   
     
     </ExpandMore> 
-    <Collapse in={expanded} timeout="auto" unmountOnExit>
+    <Collapse in={question.expanded} timeout="auto" unmountOnExit>
    
       <CardContent>
    
